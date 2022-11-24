a6427da22074ca5f423a598fa5ed9db3cd0dec55
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2022-11-23 21:30:30 -0800

    Shit's fucked

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,5 +1,59 @@
-// import { clientFeedback } from './clientFeedback.js';
+const slideArray = Array.from(document.getElementById('feedbackCarousel').children);
 
-// console.log(clientFeedback);
+const updateSlide = () => {
+    const navDots = document.getElementsByName('carouselNavDots');
+    
+    for (i = 0; i < navDots.length; i++) {
+        if (navDots[i].checked){
+            let j = navDots[i].value;
+            slideArray[j].classList.remove('hide');
+            
+            const navDotAray = Array.from(navDots);
+            let newArray = navDotAray.filter(value => value !== navDots[i]);
+            
+            slideArray[newArray[0].getAttribute("value")].classList.add('hide');
+            slideArray[newArray[1].getAttribute("value")].classList.add('hide');
 
-// const 
+
+        }
+    }
+}
+
+document.getElementsByName("carouselNavDots").forEach(element => element.addEventListener("input", updateSlide));
+
+// Left and right buttons
+// const leftNav = document.getElementById('feedbackCarouselNavLeft');
+// const rightNav  = document.getElementById('feedbackCarouselNavRight');
+
+const changeRadioRight = () => {
+    const navDots = document.getElementsByName('carouselNavDots');
+    // console.log(navDots.length);
+    for (i = 0; i < navDots.length; i++) {
+        if (navDots[i].checked && navDots[i].getAttribute('value') < navDots.length - 1){
+            document.getElementById(`feedbackCarouselNav${i+1}`).checked = true;
+            updateSlide();
+        } else if (navDots[i].getAttribute('value') == navDots.length - 1) {
+            document.getElementById('feedbackCarouselNav0').checked = true;
+            updateSlide();
+        }
+    }
+}
+
+// const changeRadioLeft = () => {
+//     const navDots = document.getElementsByName('carouselNavDots');
+//     // console.log(navDots.length);
+//     for (i = 0; i < navDots.length; i++) {
+//         if (navDots[i].checked && navDots[i].getAttribute('value') < navDots.length - 1){
+//             console.log("true")
+//             document.getElementById(`feedbackCarouselNav${i-1}`).checked = true;
+//             updateSlide();
+//         } else if (navDots[i].getAttribute('value') == 0) {
+//             document.getElementById(`feedbackCarouselNav${navDots.length - 1}`).checked = true;
+//             updateSlide();
+//         }
+//     }
+// }
+
+// document.getElementById('feedbackCarouselNavLeft').addEventListener("click", changeRadioLeft);
+document.getElementById('feedbackCarouselNavRight').addEventListener("click", changeRadioRight);
+
