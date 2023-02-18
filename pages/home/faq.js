40744b9804a6d3da56d224187353a0151eff4f1b
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-02-18 11:47:29 -0800

    Tweaked FAQ

--- pages/home/faq.js
+++ pages/home/faq.js
@@ -1,24 +1,20 @@
-function showFaq () {
+function showFaq() {
     const faqRadios = document.getElementsByName("faqRadio")
 
     for (i = 0; i < faqRadios.length; i++) {
         let matchingAnswer = document.getElementById(`faqAnswer${faqRadios[i].value}`)
-        if (faqRadios[i].checked){
-            // Show matching answer
-            matchingAnswer.style.maxHeight = `${matchingAnswer.scrollHeight}px`
-            matchingAnswer.style.marginBottom = "0.5em"
 
+        if (faqRadios[i].checked){
+            matchingAnswer.style.maxHeight = `${matchingAnswer.scrollHeight}px`
         } else {
-            // Hide all other answers
             matchingAnswer.style.maxHeight = 0
-            matchingAnswer.style.marginBottom = 0
         }
     }
 }
 
 document.getElementsByName("faqRadio").forEach(radio => radio.addEventListener("input", showFaq))
 
-function clearRadios () {
+function clearRadios() {
     const faqRadios = document.getElementsByName("faqRadio")
     for (i = 0; i < faqRadios.length; i++) {
         if (faqRadios[i].checked){
