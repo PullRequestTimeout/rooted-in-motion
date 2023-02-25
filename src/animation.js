edad6ce43043e2ee3f50f83c90019a8c7aff95e3
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-02-24 20:34:06 -0800

    Further tweaking fade

--- src/animation.js
+++ src/animation.js
@@ -5,7 +5,7 @@ const fadeIn = document.querySelectorAll(".fade-in")
 const options = {
     root: null, //This specifies that it is the viewport that is intersecting. A root ancestor can be specified also.
     threshhold: 1, //This is the percentage of the target element that is intersecting, expressed as a 0.0 to 1.0 == 0% to 100%.
-    rootMargin: "-80px", //Works like CSS margin (but needs to be expressed in percentage or px, as a string).
+    rootMargin: "-50px", //Works like CSS margin (but needs to be expressed in percentage or px, as a string).
 }
 
 const observer = new IntersectionObserver(function(entries, observer){
