e540fcccfc966c5a3823612c667bc714819e3162
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-02-24 17:09:29 -0800

    Tweaked intersection observer function to get it running

--- src/animation.js
+++ src/animation.js
@@ -1,42 +1,30 @@
 //|| IntersectionObserver ----------------------------------------------------------------------------------------------------------------
 
-const fadeIn = document.querySelectorAll("fade-in");
+const fadeIn = document.querySelectorAll(".fade-in")
 
 const options = {
     root: null, //This specifies that it is the viewport that is intersecting. A root ancestor can be specified also.
-    threshhold: 0, //This is the percentage of the target element that is intersecting, expressed as a ratio.
+    threshhold: 0.5, //This is the percentage of the target element that is intersecting, expressed as a ratio.
     rootMargin: "0px", //Works like CSS margin (but needs to be expressed in percentage or px, as a string).
 }
 
 const observer = new IntersectionObserver(function(entries, observer){
     entries.forEach(entry => {
         if (!entry.isIntersecting){
-            return;
+            return
         } else {
-            entry.target.classList.add("fade-animation");
-        };
-        // console.log(entry);
-        // entry.target.classList.add("fade-animation");
-        // entry.target.classList.toggle("fade-animation");
-        // Try toggling the animation every time the images enter into the page, might be annoying, 
-        // might need some tweaking to get the CSS to fire on the way in and out.
-});
-}, options);
+            // entry.target.classList.add("fade-animation")
+            console.log(entry.target)
+            
+            // entry.target.classList.toggle("fade-animation")
+            // Try toggling the animation every time the images enter into the page, might be annoying, 
+            // might need some tweaking to get the CSS to fire on the way in and out.
+        }
+        
+})
+}, options)
 
-// Function could be extracted and fired as a callback function, and the function build with ES6 practices.
-
-// Attaches the "fadeIn" observer to each img element returned with the querySelector
-fadeIn.forEach(img => {
-    observer.observe(fadeIn);
-});
-
-// Still needs to be tested. "fade-in" css selector is a marker to attach animation to element, "fade-animation" will actually trigger the effect.
-// Naming could be refined.
-
-//|| Nav Bar Retracton ----------------------------------------------------------------------------------------------------------------
-
-const toggleShowNav = () => {
-    
-}
-
-document.getElementById("navbar").addEventListener("scroll", toggleShowNav)
+// Attaches the observer to each element returned by fadeIn, which is all elements with 'fade-in' class.
+fadeIn.forEach(fade => {
+    observer.observe(fade)
+})
