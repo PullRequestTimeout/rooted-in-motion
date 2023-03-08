bb16cec02643908f6767012b32630203a9fdae0b
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-03-07 18:10:40 -0800

    Connected modal popup to symptom contents

--- src/animation.js
+++ src/animation.js
@@ -1,4 +1,4 @@
-// IntersectionObserver ----------------------------------------------------------------------------------------------------------------
+// IntersectionObserver ---------------------------------------------------------------------------
 
 const fadeIn = document.querySelectorAll(".fade-in")
 
@@ -28,7 +28,7 @@ fadeIn.forEach(fade => {
     observer.observe(fade)
 })
 
-// Modal ----------------------------------------------------------------------------------------------------------------
+// Modal ------------------------------------------------------------------------------------------
 
 // Pass in null to title arg if no title is needed
 function showModal(title, content) {
@@ -53,3 +53,54 @@ function hideModal() {
 }
 
 document.getElementById("modalClose").addEventListener("click", hideModal)
+
+// Symptoms Modal Content ----------------------------------------------------------------------------------
+
+function symptomCardContents(id) {
+    // console.log(id)
+    const symptomContents1 = "Manual therapy works with muscles, joints, and fascia (connective tissue surrounding muscles, joints, and organs) of the head, neck, and spine and may help decrease the frequency of headaches and migraines and may help with neurological symptoms like tinnitus, vertigo, and insomnia and help with rehabilitation after concussions."
+    const symptomContents2 = "Manual therapy can help with rehabilitation after car accidents, whiplash injuries, falls, fractures, concussions, and treatment of scar tissue post surgery."
+    const symptomContents3 = "Manual therapy may benefit injuries associated with contact sports and overuse, rehabilitation after ligament tears, sprains, and tendon or capsule injuries."
+    const symptomContents4 = "Treatment may benefit those with scoliosis, disc injuries and herniations, arthritis, sciatica, restless leg syndrome,  people working in jobs with repetitive movements or requiring long periods of sitting, falls on the back, hips, or tailbone."
+    const symptomContents5 = "Treatment may help decrease inflammation, improve circulation surrounding affected joints, and decrease muscle tightness and is safe for those with osteoarthritis and rheumatoid arthritis."
+    const symptomContents6 = "Manual therapy may help loosen restricted or tight muscles, joints, and fascia (connective tissue surrounding muscles, joints, and organs) and can lubricate joint spaces and decrease inflammation. I work with clients who have connective tissue disorders, joint inflammation (osteoarthritis and rheumatoid arthritis), plantar fasciitis, frozen shoulder, bursitis, numbness and tingling in the extremities, muscle spasms, and carpal tunnel syndrome."
+    const symptomContents7 = "Manual therapy can help with be related to tight muscles in the face, neck, shoulders, and pelvis and can be related to teeth grinding, history of dental work or dental surgeries, and frequent sinus or ear infections."
+    const symptomContents8 = "Manual therapy may help with bloating, gas, cramping, hiatal hernias, acid reflux, indigestion, and digestive motility issues like constipation or diarrhea."
+    const symptomContents9 = "Manual therapy may help with PMS, abdominal and back pain during menses, PCOS, and endometriosis."
+    const symptomContents10 = "I work with clients of all ages and stages of life including perinatal (prenatal and postnatal) clients, newborns, and pediatric clients. Treatments can help prepare the body for labor and work with symptoms associated with pregnancy like muscle and joint pain, nausea, and swelling in the legs. In the postpartum period I work with people who are having breastfeeding difficulties, and babies struggling with gastrointestinal concerns like reflux, gas, and colic or musculoskeletal concerns like torticollis or plagiocephaly. Manual therapy can also help reduce stress and induce relaxation."
+
+    switch (id) {
+        case "symptomCard1":
+            showModal("Headaches and Neurological Conditions", symptomContents1)
+            break;
+        case "symptomCard2":
+            showModal("Traumatic Injuries and Whiplash", symptomContents2)
+            break;
+        case "symptomCard3":
+            showModal("Sports Injuries", symptomContents3)
+            break;
+        case "symptomCard4":
+            showModal("Back or Neck Pain", symptomContents4)
+            break;
+        case "symptomCard5":
+            showModal("Arthritis", symptomContents5)
+            break;
+        case "symptomCard6":
+            showModal("Joint or Muscle Pain", symptomContents6)
+            break;
+        case "symptomCard7":
+            showModal("TMJ Pain", symptomContents7)
+            break;
+        case "symptomCard8":
+            showModal("Digestive Concerns", symptomContents8)
+            break;
+        case "symptomCard9":
+            showModal("Women's Health Concerns", symptomContents9)
+            break;
+        case "symptomCard10":
+            showModal("And more!", symptomContents10)
+            break;
+    }
+}
+
+document.querySelectorAll(".client-symptoms__card").forEach(card => card.addEventListener("click", () => {symptomCardContents(card.id)}))
