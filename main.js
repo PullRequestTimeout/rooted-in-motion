3419a675114e7b46bb12be733663720ceabb37db
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-03-11 07:57:09 -0800

    :bulb: Added comments and fixed typos

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 //                ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ @PullRequestTimeout
 // Navbar -----------------------------------------------------------------------------------------
 
+// Event listeners
 document.getElementById("navCheckbox").addEventListener("input", changeNavStyle)
 window.addEventListener("scroll", navbarOpacity)
 document.body.onload = closeNav()
@@ -82,6 +83,9 @@ document.querySelectorAll(".fade-in").forEach(fade => {observer.observe(fade)})
 
 // Modal ------------------------------------------------------------------------------------------
 
+// Event listeners
+document.getElementById("modalClose").addEventListener("click", hideModal)
+
 // Pass in null to title arg if no title is needed
 function showModal(title, content) {
     const modal = document.getElementById("modal")
@@ -104,18 +108,20 @@ function hideModal() {
     document.getElementById("modalContent").classList.add("hide")
 }
 
-document.getElementById("modalClose").addEventListener("click", hideModal)
-
 // Symptoms Modal Content ----------------------------------------------------------------------------------
 
+// Event listeners
+document.querySelectorAll(".client-symptoms__card").forEach(card => card.addEventListener("click", () => {symptomCardContents(card.id)}))
+
 function symptomCardContents(id) {
+    // This is probably really attrocious accessibility practices, but it really doesn't add that much to the SEO or info conveyed on the site
     const symptomContents1 = "Manual therapy works with muscles, joints, and fascia (connective tissue surrounding muscles, joints, and organs) of the head, neck, and spine, may help decrease the frequency of headaches and migraines, and may help with neurological symptoms like tinnitus, vertigo, and insomnia and help with rehabilitation after concussions."
     const symptomContents2 = "Manual therapy can help with rehabilitation after car accidents, whiplash injuries, falls, fractures, concussions, and treatment of scar tissue post surgery."
     const symptomContents3 = "Manual therapy may benefit injuries associated with contact sports and overuse, rehabilitation after ligament tears, sprains, and tendon or capsule injuries."
     const symptomContents4 = "Treatment may benefit those with scoliosis, disc injuries and herniations, arthritis, sciatica, restless leg syndrome,  people working in jobs with repetitive movements or requiring long periods of sitting, falls on the back, hips, or tailbone."
     const symptomContents5 = "Treatment may help decrease inflammation, improve circulation surrounding affected joints, and decrease muscle tightness, and is safe for those with osteoarthritis and rheumatoid arthritis."
     const symptomContents6 = "Manual therapy may help loosen restricted or tight muscles, joints, and fascia (connective tissue surrounding muscles, joints, and organs) and can lubricate joint spaces and decrease inflammation. I work with clients who have connective tissue disorders, joint inflammation (osteoarthritis and rheumatoid arthritis), plantar fasciitis, frozen shoulder, bursitis, numbness and tingling in the extremities, muscle spasms, and carpal tunnel syndrome."
-    const symptomContents7 = "Manual therapy can help with be related to tight muscles in the face, neck, shoulders, and pelvis and can be related to teeth grinding, history of dental work or dental surgeries, and frequent sinus or ear infections."
+    const symptomContents7 = "Manual therapy can be related to tight muscles in the face, neck, shoulders, and pelvis and can be related to teeth grinding, history of dental work or dental surgeries, and frequent sinus or ear infections."
     const symptomContents8 = "Manual therapy may help with bloating, gas, cramping, hiatal hernias, acid reflux, indigestion, and digestive motility issues like constipation or diarrhea."
     const symptomContents9 = "Manual therapy may help with PMS, abdominal and back pain during menses, PCOS, and endometriosis."
     const symptomContents10 = "I work with clients of all ages and stages of life including perinatal (prenatal and postnatal) clients, newborns, and pediatric clients. Treatments can help prepare the body for labor and work with symptoms associated with pregnancy like muscle and joint pain, nausea, and swelling in the legs. In the postpartum period I work with people who are having breastfeeding difficulties, and babies struggling with gastrointestinal concerns like reflux, gas, and colic or musculoskeletal concerns like torticollis or plagiocephaly. Manual therapy can also help reduce stress and induce relaxation."
@@ -154,8 +160,6 @@ function symptomCardContents(id) {
     }
 }
 
-document.querySelectorAll(".client-symptoms__card").forEach(card => card.addEventListener("click", () => {symptomCardContents(card.id)}))
-
 // Carousel ---------------------------------------------------------------------------------------
 
 // Always starts carousel on first slide
@@ -163,7 +167,7 @@ if(document.getElementById("feedbackCarouselNav0")) {
     document.getElementById("feedbackCarouselNav0").checked = true
 }
 
-// Carousel navs
+// Event listeners -- Carousel Nav
 if (document.getElementById("feedbackCarousel")) {
     document.getElementsByName("carouselNavDots").forEach(element => element.addEventListener("input", updateSlide));
     document.getElementById('feedbackCarouselNavLeft').addEventListener("click", changeRadioLeft)
@@ -273,7 +277,7 @@ function inputConsult() {
     if (window.location.href.includes("/pages/contact.html#consult")) {
         console.log("true")
         contactFormSubject.value = "Consultation"
-        contactFormMessage.innerText = "Hi Michelle! I'd love to organise a time with you for a consultation call."
+        contactFormMessage.innerText = "Hi Michelle! I'd love to organize a time with you for a consultation call."
         contactFormName.focus()
         let offset = document.querySelector(".info").offsetHeight
         setTimeout(() => {document.body.scrollTop = document.documentElement.scrollTop = offset}, 100)
