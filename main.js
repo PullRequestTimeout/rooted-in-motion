8c29ec51e21409f6897d8382e857f0ab73a97aeb
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2023-03-11 08:04:53 -0800

    :lipstick: Sped up fade-in animation

--- main.js
+++ main.js
@@ -64,8 +64,8 @@ function navbarOpacity() {
 
 const intersectionObserverOptions = {
     root: null, //This specifies that it is the viewport that is intersecting. A root ancestor can be specified also.
-    threshhold: 0.75, //This is the percentage of the target element that is intersecting, expressed as a 0.0 to 1.0 == 0% to 100%.
-    rootMargin: "-25px", //Works like CSS margin (but needs to be expressed in percentage or px, as a string).
+    threshhold: 0.2, //This is the percentage of the target element that is intersecting, expressed as a 0.0 to 1.0 == 0% to 100%.
+    rootMargin: "0", //Works like CSS margin (but needs to be expressed in percentage or px, as a string).
 }
 
 const observer = new IntersectionObserver(entries => {
